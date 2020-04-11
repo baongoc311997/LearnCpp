0cae06f254340bdb0a6d9498fc5429d3916e9232
Quang Bảo Ngọc Nguyễn <baongoc311997@gmail.com>
2020-04-11 19:16:43 +0700

    first commit

---
+++ Test/Test1/Test1.cpp
@@ -0,0 +1,19 @@
+// Test1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+
+	double zero{ 0.0 };
+	double posinf{ 5.0 / zero }; // positive infinity
+	std::cout << posinf << std::endl;
+
+	double neginf{ -5.0 / zero }; // negative infinity
+	std::cout << neginf << std::endl;
+
+	double nan{ zero / zero }; // not a number (mathematically invalid)
+	std::cout << nan << std::endl;
+}
